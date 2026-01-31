39fef38c5fe0c9e82e1e5eaa195327a02987769f
qrs-programmer <q_shanno@live.concordia.ca>
2026-01-31 11:50:09 -0500

    chore: update eslint.config.js to fix line ending issues

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -18,7 +18,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     config: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY, 
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
     },
   },
   android: {
--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -2,11 +2,30 @@
 import expoConfig from "eslint-config-expo/flat.js";
 import esLintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
-  expoConfig,
+  ...expoConfig,
   esLintPluginPrettierRecommended,
   {
     ignores: ["dist/*", "node_modules/*"],
+  },
+  {
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+  {
+    files: ["app.config.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
   },
 ]);

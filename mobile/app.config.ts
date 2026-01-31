3437dbaf3db0a5a29add03f0091ab7005a424c53
qrs-programmer <q_shanno@live.concordia.ca>
2026-01-31 11:44:50 -0500

    chore: replace app.json with app.config.ts to use dynamic logic for injecting Google Maps API Key

---
+++ mobile/app.config.ts
@@ -0,0 +1,41 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+import "dotenv/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "mobile",
+  slug: "mobile",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    config: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY, 
+    },
+  },
+  android: {
+    package: "com.yourteam.mobile",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+});

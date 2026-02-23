63b7550df7e3ea70d2d192865d12307794cd3338
François Martinez <160101566+FrancoisMartinez@users.noreply.github.com>
2026-02-23 13:24:55 -0500

    Fix: Fix esLint warning, limit its scope and add a pre-commit hook to run linter (#93)
    * fix eslint warning and limit lint scope
    
    * add pre commit linter
    
    * add pre commit linter
    
    * add pre commit linter

--- mobile/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-export const overrides = [
-  {
-    // Test files only
-    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-    extends: ["plugin:testing-library/react"],
-  },
-];
